ed7eff9de61b6bde2e846255ea4a555e5c1a2e3d
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 09:31:55 +0100

    Initial commit for FeeRegistrationPortal

---
+++ src/main/kotlin/com/pwb/feeRegistrationPortal/utility/constant/PrivilegeConstant.kt
@@ -0,0 +1,11 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T08:30:15.495280900
+package com.pwb.feeRegistrationPortal.utility.constant
+
+object PrivilegeConstant {
+    const val CREATE = "CREATE"
+    const val UPDATE = "UPDATE"
+    const val READ = "READ"
+    const val DELETE = "DELETE"
+    const val CHECKER = "CHECKER"
+    const val ALL = "*"
+}
