7a4e4a9399e437ab3058cdb355fc198aa0fd2cc8
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 09:31:54 +0100

    Initial commit for FeeRegistrationPortal

---
+++ src/main/kotlin/com/pwb/feeRegistrationPortal/utility/constant/PrefixConstant.kt
@@ -0,0 +1,6 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T08:30:15.495280900
+package com.pwb.feeRegistrationPortal.utility.constant
+
+object PrefixConstant {
+    const val ENTITY = "DBO."
+}
