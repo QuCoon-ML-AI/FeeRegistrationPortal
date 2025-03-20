96512ca3e438c24b20b78059b48e8fca3a659bdd
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 09:32:02 +0100

    Initial commit for FeeRegistrationPortal

---
+++ src/test/kotlin/com/pwb/feeRegistrationPortal/FeeRegistrationPortalApplicationTests.kt
@@ -0,0 +1,15 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T08:30:15.542134800
+package com.pwb.feeRegistrationPortal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FeeRegistrationPortalApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
+
